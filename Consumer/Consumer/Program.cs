f8802716c5fc7f374720b9d56ad0f9c930c5a88c
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2019-02-05 21:41:05 +0100

    routing with logs

--- Consumer/Consumer/Program.cs
+++ Consumer/Consumer/Program.cs
@@ -15,18 +15,32 @@ namespace Consumer
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.ExchangeDeclare("logs", "fanout");
+                channel.ExchangeDeclare("direct_logs", "direct");
 
                 var queueName = channel.QueueDeclare().QueueName;
-                channel.QueueBind(queueName, "logs", "");
 
-                Console.WriteLine("# Waiting for logs.");
+                if (args.Length < 1)
+                {
+                    Console.Error.WriteLine($"Usage: {Environment.GetCommandLineArgs()[0]} [info] [warning] [error]");
+                    Console.WriteLine("# Press [enter] to exit.");
+                    Console.ReadLine();
+                    Environment.ExitCode = 1;
+                    return;
+                }
+
+                foreach (var severity in args)
+                {
+                    channel.QueueBind(queueName, "direct_logs", severity);
+                }
+
+                Console.WriteLine("# Waiting for messages.");
 
                 var consumer = new EventingBasicConsumer(channel);
                 consumer.Received += (model, ea) =>
                 {
                     var body = ea.Body;
                     var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
                     Console.Write("--> Please wait. The message is loading");
                     for (int i = 0; i < 5; i++)
                     {
@@ -37,7 +51,7 @@ namespace Consumer
                     Console.WriteLine();
                     Console.WriteLine("# Done.");
                     Console.WriteLine();
-                    Console.WriteLine($"--> Received: {message}");
+                    Console.WriteLine($"--> Received: {routingKey} : {message}");
                 };
 
                 channel.BasicConsume(queueName, true, consumer);
--- Producer/Producer/Program.cs
+++ Producer/Producer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Text;
 using System.Threading;
 using RabbitMQ.Client;
@@ -9,29 +10,30 @@ namespace Producer
     {
         static void Main(string[] args)
         {
-            var factory = new ConnectionFactory { HostName = "localhost"};
+            var factory = new ConnectionFactory { HostName = "localhost" };
 
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
                 {
-                    channel.ExchangeDeclare("logs", "fanout");
+                    channel.ExchangeDeclare("direct_logs", "direct");
 
-                    string message = GetMessage(args);
+                    var severity = (args.Length > 0) ? args[0] : "info";
+
+                    var message = (args.Length < 1)
+                        ? string.Join(" ", args.Skip(1).ToArray())
+                        : "Welcome to the trip, man!";
+
                     var body = Encoding.UTF8.GetBytes(message);
 
-                    channel.BasicPublish("logs", "", null, body);
+                    channel.BasicPublish("direct_logs", severity, null, body);
+
                     Console.WriteLine($"--> Sent: {message}");
                 }
 
                 Console.WriteLine("# Press [enter] to exit.");
                 Console.ReadLine();
             }
-        }
-
-        private static string GetMessage(string[] args)
-        {
-            return ((args.Length > 0) ? string.Join(" ", args) : "Welcome to the trip, man!");
         }
     }
 }
