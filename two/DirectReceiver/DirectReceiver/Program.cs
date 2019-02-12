79a87c988e911c224cfa21869f080489ae39da25
Aleksandra Stocka <aleksandra.stocka689@gmail.com>
2019-02-12 20:14:32 +0100

    add project - direct exchange

---
+++ two/DirectReceiver/DirectReceiver/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace DirectReceiver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Direct Message Receiver App --\r\n");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_exchange", type: "direct");
+                channel.QueueBind(queue: "messages", exchange: "direct_exchange", routingKey: "direct_message");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    var routingKey = ea.RoutingKey;
+                    Console.WriteLine($">> [{routingKey}] Receive: {message}");
+                };
+
+                channel.BasicConsume(queue: "messages", autoAck: true, consumer: consumer);
+
+                Console.WriteLine("Press [enter] to exit.\r\n");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ two/DirectSender/DirectSender/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace DirectSender
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-- Direct Message Sender App --\r\n");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "direct_exchange", type: "direct");
+                channel.QueueDeclare(queue: "messages", durable: false, exclusive: false, autoDelete: false,
+                    arguments: null);
+                var routingKey = "direct_message";
+
+                Console.Write(">> ");
+                var message = Console.ReadLine();
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "direct_exchange", routingKey: routingKey, basicProperties: null,
+                    body: body);
+
+                Console.WriteLine($"<< [{routingKey}] Sent: {message}");
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
